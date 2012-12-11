e68c773ec9312107fd317b59d9fa017c2f760154
borislav <b.manolov@gmail.com>
2012-12-11 20:36:36 +0100

    rm vendor tests

--- vendor/jms/serializer-bundle/JMS/SerializerBundle/Tests/Metadata/Driver/php/AuthorReadOnly.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use JMS\SerializerBundle\Metadata\ClassMetadata;
-use JMS\SerializerBundle\Metadata\PropertyMetadata;
-
-$metadata = new ClassMetadata('JMS\SerializerBundle\Tests\Fixtures\AuthorReadOnly');
-
-return $metadata;
