67031fa29e1551774e1415c9a11081b445390202
borislav <b.manolov@gmail.com>
2012-12-25 17:23:39 +0100

    custom very simple cache

--- web/index.php
+++ web/index.php
@@ -1,4 +1,69 @@
 <?php
+function isCacheable() {
+	return !array_key_exists('mlt', $_COOKIE);
+}
+class Cache {
+	private $file;
+	private $request;
+	private $ttl = 10;
+	private $debug = false;
+
+	public function __construct($requestUri, $cacheDir) {
+		$hash = md5($requestUri);
+		$this->file = "$cacheDir/$hash[0]/$hash[1]/$hash[2]/$hash";
+		$this->request = $requestUri;
+	}
+
+	public function get() {
+		if (file_exists($this->file)) {
+			if ($this->isFresh()) {
+				$this->log("=== CACHE HIT");
+				return file_get_contents($this->file);
+			}
+			$this->purge();
+		}
+		return null;
+	}
+	public function getTtl() {
+		return $this->ttl;
+	}
+	private function isFresh() {
+		$origTtl = file_get_contents("$this->file.ttl") + rand(0, 30);
+		$age = time() - filemtime($this->file);
+		$this->ttl = $origTtl - $age;
+		return $this->ttl > 0;
+	}
+	public function set($content, $ttl) {
+		if ( ! $ttl) {
+			return;
+		}
+		$cacheDir = dirname($this->file);
+		if (!file_exists($cacheDir)) {
+			mkdir($cacheDir, 0777, true);
+		}
+		file_put_contents($this->file, $content);
+		file_put_contents("$this->file.ttl", $ttl);
+		$this->log("+++ CACHE MISS ($ttl)");
+	}
+	private function purge() {
+		unlink($this->file);
+		unlink("$this->file.ttl");
+		$this->log('--- CACHE PURGE');
+	}
+	private function log($msg) {
+		if ($this->debug) {
+			error_log("$msg - $this->request");
+		}
+	}
+}
+$cache = new Cache($_SERVER['REQUEST_URI'], __DIR__.'/../app/cache/prod/simple_http_cache');
+
+if (isCacheable() && null !== ($cachedContent = $cache->get())) {
+	header("Cache-Control: public, max-age=".$cache->getTtl());
+	echo $cachedContent;
+	return;
+}
+
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -14,7 +79,7 @@ try {
 }
 
 require_once $rootDir.'/app/AppKernel.php';
-require_once $rootDir.'/app/AppCache.php';
+//require_once $rootDir.'/app/AppCache.php';
 
 register_shutdown_function(function(){
 	$error = error_get_last();
@@ -22,6 +87,11 @@ register_shutdown_function(function(){
 		if (preg_match('/parameters\.yml.+does not exist/', $error['message'])) {
 			header('Location: /install.php');
 			exit;
+		}
+		if (strpos($error['message'], 'Out of memory') === false) {
+			file_put_contents(__DIR__.'/../app/logs/fatal-error.log',
+				"\nFATAL ERROR\nrequest = $_SERVER[REQUEST_URI]\n"
+				. print_r($error, true)."\n", FILE_APPEND);
 		}
 		ob_clean();
 		header('HTTP/1.1 503 Service Unavailable');
@@ -31,8 +101,11 @@ register_shutdown_function(function(){
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
-$kernel = new AppCache($kernel);
+//$kernel = new AppCache($kernel);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
+if (isCacheable() && $response->isOk()) {
+	$cache->set($response->getContent(), $response->getTtl());
+}
 $response->send();
 $kernel->terminate($request, $response);
