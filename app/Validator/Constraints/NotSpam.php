9ecb3ae76eeb35fab9c40e60238d377dd8e411f4
borislav <b.manolov@gmail.com>
2022-10-12 18:31:16 +0200

    Enhance spam validator

--- app/Validator/Constraints/NotSpam.php
+++ app/Validator/Constraints/NotSpam.php
@@ -8,9 +8,19 @@ use Symfony\Component\Validator\Constraint;
 class NotSpam extends Constraint {
 	public $message = 'notspam';
 	public $urlLimit = 2;
-	public $stopWords = [
-		'CASH & CARRY',
-		'НЕ ОТВЕЧАЙТЕ НА ЭТО ПИСЬМО',
-		' еба ',
-	];
+	public $stopWords = [];
+	public $stopWordsFile = __DIR__.'/../../config/spam_phrases.ini';
+
+	public function __construct($options = null, string $stopWordsFile = null) {
+		parent::__construct($options);
+		$this->stopWordsFile = $stopWordsFile ?? $this->stopWordsFile;
+		$this->stopWords = $this->fetchStopWords();
+	}
+
+	private function fetchStopWords() {
+		$lines = file($this->stopWordsFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+		return array_filter($lines, function(string $line) {
+			return $line[0] !== '#';
+		});
+	}
 }
